c7643331b97b4dfa30e43e3c9661b4d535f5355c
Vidya-Naroju <vidya.naroju@gmail.com>
2017-08-02 19:51:46 +0100

    adding dynamic double decimal values

---
+++ src/java_Homework/AdditionOfDecimal.java
@@ -0,0 +1,20 @@
+package java_Homework;
+
+import java.util.Scanner;
+
+public class AdditionOfDecimal {
+
+	public static void main(String[] args) {
+		 double a,b,sum;
+		 Scanner s=new Scanner(System.in);
+		
+		 System.out.println("enter the value of a : ");
+		a=s.nextDouble();
+		 System.out.println("enter the value of b : ");
+		b= s.nextDouble();
+		 System.out.println(sum=a+b);
+		 
+
+	}
+
+}
