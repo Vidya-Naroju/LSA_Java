a4e7eb6454011c1f01eaa2d9077d23337fff6cab
Vidya-Naroju <vidya.naroju@gmail.com>
2017-08-02 20:26:06 +0100

    Increment_decrement program

---
+++ src/java_Homework/Increment_Decrement.java
@@ -0,0 +1,23 @@
+package java_Homework;
+
+public class Increment_Decrement {
+	int a=5;
+	 
+	public void values(){
+		//System.out.println("a :"+a);
+		System.out.println("a++ :"+ a++); 
+		//System.out.println("++a :"+ (++a));
+		//System.out.println("a+=a :"+(a+=a));
+		//System.out.println("a-- :"+ (a--));
+		//System.out.println("--a :"+ (--a));
+		//System.out.println("a-=a :"+ (a-=a));
+		
+	}
+	public static void main(String[] args) {
+		Increment_Decrement id=new Increment_Decrement();
+		
+	id.values();
+
+	}
+
+}
